9584ecfe64f500f56e5a81fddb5a34b676ee688d
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-15 11:53:07 -0600

    submitted posts persist on page

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ app.controller('postController', function ($scope) {
   $scope.posts = [];
   $scope.submitPost = function () {
     $scope.posts.push({title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description})
+      $scope.title = "";
+      $scope.author = "";
+      $scope.url = "";
+      $scope.description = "";
+
   }
 }) //end of app
