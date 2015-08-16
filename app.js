ca8f0daf8b199f85551e3d7d62885c47f1b23170
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-16 15:57:49 -0600

    added comment feature and angularMoment

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-var app = angular.module("redditClone", []);
+var app = angular.module("redditClone", ['angularMoment'])
 
 app.controller('postController', function ($scope) {
   $scope.posts = [];
   $scope.submitPost = function () {
-    $scope.posts.push({title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description})
+    $scope.vm = this;
+    $scope.vm.time = new Date();
+    $scope.date = $scope.vm.time;
+    $scope.posts.push({title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date})
       $scope.title = "";
       $scope.author = "";
       $scope.url = "";
       $scope.description = "";
-      $scope.rightNow = new Date;
   }
   $scope.comments =[]
   $scope.submitComment = function () {
     $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText})
       $scope.commentAuthor = "";
       $scope.commentText = ""
-
   }
+
+
 }) //end of app
