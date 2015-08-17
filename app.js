03f11b43183515f9b5638dd02a1cc9120fab0c54
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-17 11:52:08 -0600

    comments feature is working

--- app.js
+++ app.js
@@ -6,23 +6,29 @@ app.controller('postController', function ($scope) {
     $scope.vm = this;
     $scope.vm.time = new Date();
     $scope.date = $scope.vm.time;
-    $scope.posts.push({title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date})
+    $scope.posts.push(
+      {title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date})
       $scope.title = "";
       $scope.author = "";
       $scope.url = "";
       $scope.description = "";
   }
+}) //end of postController
+
+app.controller('commentController', function ($scope) {
+  $scope.comments =[]
+  $scope.submitComment = function () {
+    $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText})
+      $scope.commentAuthor = "";
+      $scope.commentText = ""  }
+}) //end of commentController
+
+app.controller('voteController', function ($scope) {
+  $scope.vote = 0;
   $scope.upVote = function(){
     $scope.vote++;
   }
   $scope.downVote = function() {
     $scope.vote--;
   }
-  $scope.vote = 0;
-  $scope.comments =[]
-  $scope.submitComment = function () {
-    $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText})
-      $scope.commentAuthor = "";
-      $scope.commentText = ""
-  }
-}) //end of app
+}) //end of voteController
