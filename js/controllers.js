20dddf6cd3b7f176da2600ffb6e459af046fcb13
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-27 11:54:56 -0600

    refactoring with Firebase

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,20 @@
 app.controller('postController', function ($scope, $firebaseArray) {
   var postsRef = new Firebase("https://schles-reddit-clone.firebaseio.com/posts");
   $scope.posts = $firebaseArray(postsRef);
-  
+  $scope.newPost  = {title: 'testing', author: 'Kim'}
+  //Hard coded values work, but dynamic ones do not. Solve this problem next! 
+  // {title: '$scope.title', author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date};
+
   $scope.submitPost = function () {
-    $scope.vm = this;
-    $scope.vm.time = new Date();
-    $scope.date = $scope.vm.time;
-    $scope.posts.push(
-      {title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date});
+    // $scope.vm = this;
+    // $scope.vm.time = new Date();
+    // $scope.date = $scope.vm.time;
+    $scope.posts.$add($scope.newPost).then(function(data) {
       $scope.title = '';
       $scope.author = '';
       $scope.url = '';
       $scope.description = '';
+    })
   };
 }); //end of postController
 
