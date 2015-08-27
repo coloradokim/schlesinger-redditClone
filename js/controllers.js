485b50bc7ff43dbd59f689da06a1aa7e299c2e6c
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-27 11:24:43 -0600

    refactoring app to use Firebase

---
+++ js/controllers.js
@@ -0,0 +1,35 @@
+app.controller('postController', function ($scope, $firebaseArray) {
+  var postsRef = new Firebase("https://schles-reddit-clone.firebaseio.com/posts");
+  $scope.posts = $firebaseArray(postsRef);
+  
+  $scope.submitPost = function () {
+    $scope.vm = this;
+    $scope.vm.time = new Date();
+    $scope.date = $scope.vm.time;
+    $scope.posts.push(
+      {title: $scope.title, author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date});
+      $scope.title = '';
+      $scope.author = '';
+      $scope.url = '';
+      $scope.description = '';
+  };
+}); //end of postController
+
+app.controller('commentController', function ($scope) {
+  $scope.comments =[];
+  $scope.submitComment = function () {
+    $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText});
+      $scope.commentAuthor = '';
+      $scope.commentText = '';
+  };
+}); //end of commentController
+
+app.controller('voteController', function ($scope) {
+  $scope.post.vote = 0;
+  $scope.upVote = function(){
+    $scope.post.vote++;
+  };
+  $scope.downVote = function() {
+    $scope.post.vote--;
+  };
+}); //end of voteController
