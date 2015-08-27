b8d5c4440c6e7b990b15e52c3a926c5be5811548
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-27 17:45:33 -0600

    added submit post feature

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,7 @@
 app.controller('postController', function ($scope, $firebaseArray) {
   var postsRef = new Firebase("https://schles-reddit-clone.firebaseio.com/posts");
   $scope.posts = $firebaseArray(postsRef);
-  $scope.newPost  = {title: 'testing', author: 'Kim'}
-  //Hard coded values work, but dynamic ones do not. Solve this problem next! 
-  // {title: '$scope.title', author: $scope.author, url: $scope.url, description: $scope.description, date: $scope.date};
+  $scope.newPost  = {title: "", author: "", url: "", description: "", date: ""};
 
   $scope.submitPost = function () {
     // $scope.vm = this;
@@ -16,16 +14,26 @@ app.controller('postController', function ($scope, $firebaseArray) {
       $scope.description = '';
     })
   };
+  $scope.addComment = function (post, commentAuthor, commentText) {
+    if (post.comments === undefined) {
+      post.comments = [];
+    }
+    var commentObject = {};
+    commentObject.commentAuthor = commentAuthor
+    commentObject.commentText = commentText
+    post.comments.push(commentObject)
+    $scope.posts.$save(post)
+  }
 }); //end of postController
 
-app.controller('commentController', function ($scope) {
-  $scope.comments =[];
-  $scope.submitComment = function () {
-    $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText});
-      $scope.commentAuthor = '';
-      $scope.commentText = '';
-  };
-}); //end of commentController
+// app.controller('commentController', function ($scope) {
+  // $scope.comments =[];
+  // $scope.submitComment = function () {
+  //   $scope.comments.push({commentAuthor: $scope.commentAuthor, commentText: $scope.commentText});
+  //     $scope.commentAuthor = '';
+  //     $scope.commentText = '';
+  // };
+// }); //end of commentController
 
 app.controller('voteController', function ($scope) {
   $scope.post.vote = 0;
